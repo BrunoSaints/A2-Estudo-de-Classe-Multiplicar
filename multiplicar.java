1250e847fbbd4fa0aff66c5dd61a4f92655ba022
Bruno <brunos422@gmail.com>
2022-07-04 14:20:17 -0300

    Multiplicar números

--- multiplicar.java
+++ multiplicar.java
@@ -10,7 +10,8 @@ public class multiplicar {
         return a * b * c;
     }
     public static void main(String[] args) {
-        System.out.println("Mult: " + multiplicar.mult(5, 3));
-       
+        System.out.println("Resultado: " + multiplicar.mult(5, 3));
+        System.out.println("Resultado: " + multiplicar.mult(10, 3, 8));  
+        System.out.println("Resultado: " + multiplicar.mult(4.5, 3.8, 2));  
     }
 }
